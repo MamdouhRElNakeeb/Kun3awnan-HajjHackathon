be56de36eba17a00a6777b3c2876abb5e3465a4a
Mamdouh El Nakeeb <mamdouh..el.nakeeb@gmail.com>
2018-08-01 23:22:44 +0200

    inital commit

---
+++ app/src/main/java/com/kun3awnan/app/activity/Login.java
@@ -0,0 +1,15 @@
+package com.kun3awnan.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kun3awnan.app.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
---
+++ app/src/main/java/com/kun3awnan/app/activity/Signup.java
@@ -0,0 +1,15 @@
+package com.kun3awnan.app.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kun3awnan.app.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
+}
