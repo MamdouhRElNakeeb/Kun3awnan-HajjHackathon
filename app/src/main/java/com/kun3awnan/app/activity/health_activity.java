faeb43cdbb976cf08b5e366bb38bfa9848036637
noorio <noorio@aucegypt.edu>
2018-08-02 23:56:46 +0200

    Health, Home Services Layouts

---
+++ app/src/main/java/com/kun3awnan/app/activity/health_activity.java
@@ -0,0 +1,16 @@
+package com.kun3awnan.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kun3awnan.app.R;
+
+
+public class health_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health_activity);
+    }
+}
