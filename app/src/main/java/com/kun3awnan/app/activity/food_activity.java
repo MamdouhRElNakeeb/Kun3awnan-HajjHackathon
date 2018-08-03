a07979afc1a43af35a617777df6e6686f8142549
refaay <refaay@aucegypt.edu>
2018-08-03 05:58:23 +0300

    Housing Java, food_activity

--- app/src/main/java/com/kun3awnan/app/activity/food_activity.java
+++ app/src/main/java/com/kun3awnan/app/activity/food_activity.java
@@ -1,13 +1,39 @@
 package com.kun3awnan.app.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+
+import com.kun3awnan.app.R;
 
 public class food_activity extends AppCompatActivity {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+
+
+    private TextView toolbar_text;
+    private ListView listView;
+    private FloatingActionButton floatingActionButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_activity);
+
+
+        toolbar_text = (TextView) findViewById(R.id.toolbarText);
+        listView = (ListView) findViewById(R.id.food_lv);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(food_activity.this, food_form.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/kun3awnan/app/activity/health_activity.java
+++ app/src/main/java/com/kun3awnan/app/activity/health_activity.java
@@ -1,31 +1,70 @@
 package com.kun3awnan.app.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.kun3awnan.app.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class health_activity extends AppCompatActivity {
-
-    //View Elements Declaration
-    private LinearLayout bonesDoctor;
-    private LinearLayout heartDoctor;
-    private LinearLayout Dentist;
-    private LinearLayout publicHealth;
-
+    private FirebaseFirestore db;
+    private DocumentReference reqRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.health_activity);
-        bonesDoctor = findViewById(R.id.bones_doctor);
-        heartDoctor = findViewById(R.id.heart_doctor);
-        Dentist = findViewById(R.id.dentist);
-        publicHealth = findViewById(R.id.public_health);
 
+        db = FirebaseFirestore.getInstance();
+        reqRef = db.collection("requests").document();
+    }
+
+    public void navigateScreen(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.BonesLL:
+                saveReq();
+                break;
+
+        }
 
     }
 
 
+    public void saveReq() {
+
+        Map<String, Object> request = new HashMap<>();
+        request.put("reqCat", "Public Health");
+        request.put("reqType", "Bones");
+
+        reqRef.set(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(health_activity.this, "Request Added", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(health_activity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d("Error!", e.toString());
+                    }
+                });
+
+
+    }
 }
