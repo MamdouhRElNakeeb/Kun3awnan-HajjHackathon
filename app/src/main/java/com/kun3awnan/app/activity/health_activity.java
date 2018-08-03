78cb1d05642afa8bc5f8185d7096386f36cd5566
Mamdouh El Nakeeb <mamdouh..el.nakeeb@gmail.com>
2018-08-03 05:58:57 +0200

    send notification to health topic

--- app/src/main/java/com/kun3awnan/app/activity/health_activity.java
+++ app/src/main/java/com/kun3awnan/app/activity/health_activity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.ANRequest;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.OkHttpResponseAndJSONObjectRequestListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.kun3awnan.app.R;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -19,6 +23,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.kun3awnan.app.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Response;
 
 
 public class health_activity extends AppCompatActivity {
@@ -71,6 +80,12 @@ public class health_activity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void aVoid) {
                         Toast.makeText(health_activity.this, "Request Added", Toast.LENGTH_SHORT).show();
+
+                        try {
+                            notifyUsers();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -81,6 +96,53 @@ public class health_activity extends AppCompatActivity {
                     }
                 });
 
+
+    }
+
+    private void notifyUsers() throws JSONException {
+
+        JSONObject notifObj = new JSONObject();
+        JSONObject msgObj = new JSONObject();
+
+        JSONObject dataObj = new JSONObject();
+        dataObj.put("body", "open the message to view more details!");
+        dataObj.put("title", "Someone needs help");
+        dataObj.put("reqID", reqRef.getId());
+
+        msgObj.put("data", dataObj);
+        msgObj.put("topic", "health");
+        notifObj.put("message", msgObj);
+
+
+        ANRequest.PostRequestBuilder postRequestBuilder = AndroidNetworking.post("https://fcm.googleapis.com/fcm/send");
+
+        postRequestBuilder.addHeaders("Content-Type", "application/json")
+                .addHeaders("Authorization", "key=" + "AIzaSyC9B497SMs_at6Lj61tA7ZOScBLfiEhoQw")
+                .addJSONObjectBody(notifObj)
+                .build().
+                getAsOkHttpResponseAndJSONObject(new OkHttpResponseAndJSONObjectRequestListener() {
+
+                    @Override
+                    public void onResponse(Response okHttpResponse, JSONObject response) {
+
+                        Log.d("response", response.toString());
+
+                        try {
+                            if (response.has("success") && response.getBoolean("success")){
+                                Toast.makeText(getBaseContext(), "تم إرسال الطلب بنجاح", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                    }
+
+                });
 
     }
 
